c2db7fe84ca162fde606e16d76eb0216e9b43ece
KBiz65 <34324949+KBiz65@users.noreply.github.com>
2022-05-17 13:53:28 -0700

    added color value checks

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,29 +2,54 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../Components/ColorCounter';
 
-const COLOR_INCREMENT = 10;
+const COLOR_INCREMENT = 5;
+const COLOR_DECREMENT = -5;
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColor = (color, change) => {
+    if (color === 'red') {
+      if (red + change > 255 || red + change < 0) {
+        return;
+      } else {
+        setRed(red + change);
+      }
+    }
+    if (color === 'green') {
+      if (green + change > 255 || green + change < 0) {
+        return;
+      } else {
+        setGreen(green + change);
+      }
+    }
+    if (color === 'blue') {
+      if (blue + change > 255 || blue + change < 0) {
+        return;
+      } else {
+        setBlue(blue + change);
+      }
+    }
+  }
+
   return (
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', COLOR_DECREMENT)}
       />
       <ColorCounter
         color="Green"
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', COLOR_DECREMENT)}
       />
       <ColorCounter
         color="Blue"
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', COLOR_DECREMENT)}
       />
       <Text>Color: {`Red: ${red}, Green: ${green}, Blue: ${blue}`}</Text>
       <View
